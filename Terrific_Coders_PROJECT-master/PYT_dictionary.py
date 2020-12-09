cce7e3f04a348ed0f6a099471fb36364cdea776d
Michael Salamon <michaelsalamon78@gmail.com>
2020-12-09 18:20:20 -0500

    Trying to finish the backend!

--- Terrific_Coders_PROJECT-master/PYT_dictionary.py
+++ Terrific_Coders_PROJECT-master/PYT_dictionary.py
@@ -1,13 +1,17 @@
 import string
 from collections import *
 
+# Upon running this file, a directory must be provided to some .txt file.
 fname = input("Enter the file name: ")
 if len(fname) < 1: fname = 'words.txt'
 fh = open(fname)
 
-list = []
-biglist = []
-bigcount = 0
+
+
+#                                                   INITIALIZATIONS
+
+
+# keyword lists to classify the articles we want
 PRIMARY =   ['corona', 'coronavirus', 'covid19']# We remove all symbols like dash so it's covid19 instead of covid-19.
 SECONDARY = ['hospitals', 'cases', 'max', 'capacity', 'spreading', 'infection', 'rate', 'slower', 'faster', 'country',
              'city', 'doctors', 'staff', 'symptoms', 'spread', 'quarantine', 'trend', 'unemployment', 'surge', 'vaccine',
@@ -15,78 +19,6 @@ SECONDARY = ['hospitals', 'cases', 'max', 'capacity', 'spreading', 'infection', 
 BAD =       ['White House', 'president', 'senate', 'federal', 'government', 'democrat', 'republican', 'party', 'election',
              'Constitution', 'Supreme Court']# 'White House' and 'Supreme Court' won't add to the keyword count. 
                                              # Left as is in order to maintain formulas.
-
-def printNEAT(keyword, example):# prints 5 elements per line
-    if (len(example)) <= 5:
-        print(keyword)
-        for i in (range(len(example))):
-            print(example[i], end =" ")
-        print('\n')
-    else:
-        print(keyword)
-        rows=(len(example))//5
-        if (len(example))/5.0 > rows:
-            rows=rows+1
-        for i in range(rows*5):
-            if i>((len(example))-1):
-                break
-            elif ((i+1)%5) == 0:
-                print(example[i])
-            else:
-                print(example[i], end =" ")
-        print()
-
-
-
-printNEAT("PRIMARY:   ", PRIMARY)
-printNEAT("SECONDARY: ", SECONDARY)
-printNEAT("BAD:       ", BAD)
-
-for line in fh:
-    words = (line.rstrip().lower()).translate(str.maketrans('', '', string.punctuation)).split()
-    for word in words:
-        biglist.append(word)
-        bigcount = bigcount + 1
-        if word in list:
-            continue
-        else:
-            list.append(word)
-
-list.sort()
-biglist.sort()
-
-print('\n...THIS ARTICLE HAS', bigcount, 'WORDS...\n')  # WORD COUNT
-
-primword_freq = 0
-for i in range(len(PRIMARY)):
-    primword_freq = primword_freq + biglist.count(PRIMARY[i])
-
-
-secword_freq = 0
-for j in range(len(SECONDARY)):
-    secword_freq = secword_freq + biglist.count(SECONDARY[j])
-
-badword_freq = 0
-for k in range(len(BAD)):
-    badword_freq = badword_freq + biglist.count(BAD[k])
-
-print("\nThe number of times a PRIMARY keyword was used:   ", primword_freq)
-print("The number of times a SECONDARY keyword was used: ", secword_freq)
-print("The number of times a BAD keyword was used:       ", badword_freq)
-
-
-# Here is a formula for a good article:
-if (primword_freq>=1 and secword_freq>=6 and badword_freq<=1):
-    print("This article is good!")
-# otherwise, give interesting feedback:
-elif(primword_freq>=3 and secword_freq>=10 and badword_freq>=6):
-	print("This article is politics and corona.")
-elif(primword_freq==0 and secword_freq<=2 and badword_freq>=2):
-	print("This article is politics.")
-elif(primword_freq==0 and secword_freq==0 and badword_freq==0):
-	print("This article is irrelevant.")
-else:
-    print("This article is a mix of things.")
 
 # default websites
 preferred_websites=['http://www.Look-elsewhere-SORRY.com/',
@@ -98,6 +30,89 @@ preferred_websites=['http://www.Look-elsewhere-SORRY.com/',
                     'https://www.nbcnews.com/',
                     'https://apnews.com/',
                     'https://www.npr.org/sections/news/']
+
+
+
+#                                                   FUNCTIONS
+
+
+# "PRIMARY:   "  "SECONDARY: "  "BAD:       "
+def print_keyword_lists(content_describe1, example_list1, content_describe2, example_list2, content_describe3, example_list3):
+    printNEAT(content_describe1, example_list1)
+    printNEAT(content_describe2, example_list2)
+    printNEAT(content_describe3, example_list3)
+
+def printNEAT(content_describe, example_list):# prints 5 elements per line
+    if (len(example_list)) <= 5:
+        print(content_describe)
+        for i in (range(len(example_list))):
+            print(example_list[i], end =" ")
+        print('\n')
+    else:
+        print(content_describe)
+        rows=(len(example_list))//5
+        if (len(example_list))/5.0 > rows:
+            rows=rows+1
+        for i in range(rows*5):
+            if i>((len(example_list))-1):
+                break
+            elif ((i+1)%5) == 0:
+                print(example_list[i])
+            else:
+                print(example_list[i], end =" ")
+        print()
+
+def classify_article(example_list1, example_list2, example_list3, url):
+    list = []
+    biglist = []
+    bigcount = 0
+    for line in fh:
+        words = (line.rstrip().lower()).translate(str.maketrans('', '', string.punctuation)).split()
+        for word in words:
+            biglist.append(word)
+            bigcount = bigcount + 1
+            if word in list:
+                continue
+            else:
+                list.append(word)
+    list.sort()
+    biglist.sort()
+
+    # print('\n...THIS ARTICLE HAS', bigcount, 'WORDS...\n')  # WORD COUNT
+
+    primword_freq = 0
+    for i in range(len(PRIMARY)):
+        primword_freq = primword_freq + biglist.count(example_list1[i])
+    secword_freq = 0
+    for j in range(len(SECONDARY)):
+        secword_freq = secword_freq + biglist.count(example_list2[j])
+    badword_freq = 0
+    for k in range(len(BAD)):
+        badword_freq = badword_freq + biglist.count(example_list3[k])
+
+    # print("\nThe number of times a PRIMARY keyword was used:   ", primword_freq)
+    # print("The number of times a SECONDARY keyword was used: ", secword_freq)
+    # print("The number of times a BAD keyword was used:       ", badword_freq)
+
+
+    # Here is a formula for a good article:
+    if (primword_freq>=1 and secword_freq>=6 and badword_freq<=1):
+        print("This article is good!")
+        # return the url here!
+    # otherwise, give interesting feedback:
+    elif(primword_freq>=3 and secword_freq>=10 and badword_freq>=6):
+	    print("This article is politics and corona.")
+    elif(primword_freq==0 and secword_freq<=2 and badword_freq>=2):
+	    print("This article is politics.")
+    elif(primword_freq==0 and secword_freq==0 and badword_freq==0):
+	    print("This article is irrelevant.")
+    else:
+        print("This article is a mix of things.")
+
+
+print_keyword_lists("PRIMARY:   ",PRIMARY, "SECONDARY: ", SECONDARY, "BAD:       ", BAD)
+classify_article(PRIMARY, SECONDARY, BAD, 'http://www.Look-elsewhere-SORRY.com/')
+
 
 
 # Typical file names for input:
